0f015b413af7a8fcf1c8322d44314fee8e8ea65d
jack_sparrow <super_user.name>
2024-02-10 08:17:24 +0200

    adding barks in python

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # cute_dogs
 this repo about cute dogs that can bark  in python language !
+
+
+## smaller title
+app.py  run it to see the dogs bark ! 
---
+++ app.py
@@ -0,0 +1,11 @@
+def bark:
+    print('woof woof i just barked in python ...  !')
+
+
+
+def bark_twice():
+    bark()
+    bark()
+
+### doing the glorious barks ! 
+bark_twice()
